b8f1317706dcb343519577a026c7593ddcad50df
Francisco Edilton <franciscoed@gmail.com>
2021-07-02 15:15:02 +0100

    Lista de Imoveis

--- fii_xml.py
+++ fii_xml.py
@@ -1,10 +1,32 @@
 import xml.etree.ElementTree as ET
 import streamlit as st
 import pandas as pd
-import json
 
 tree = ET.parse('tri.xml')
 root = tree.getroot()
+tmp_dict = {}
+tmp_list = []
+# Titulo
+for elem in root.iter('NomeFundo'):
+    st.title(elem.text)
+
+# Quantidade de Imoveis
+st.header("Imoveis")
+field_list = [
+    'Nome', 'NumUnidades', 'OutrasCaractRelevantes', 'PercentVacancia', 'PercentInadimplencia', 'PercentReceitasFII',
+]
+for elem in root.iter('LstImovRendaAcabados'):
+    for subelem in elem:
+        # Reset dictionary
+        tmp_dict = {}
+        for imovel in subelem:
+            if imovel.tag in field_list:
+                tmp_dict[imovel.tag] = imovel.text
+        # add to the list
+        tmp_list.append(tmp_dict)
+
+df_imoveis = pd.DataFrame(data=tmp_list, columns=field_list)
+st.table(df_imoveis)
 
 
 # Vencimento
